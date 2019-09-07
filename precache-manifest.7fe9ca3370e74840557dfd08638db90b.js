5b2af3f061c5305c84dea8cd6cbac033deaf9466
ajinra020307 <ajinra020307@gmail.com>
2019-09-07 22:54:07 +0530

    all

---
+++ precache-manifest.7fe9ca3370e74840557dfd08638db90b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3004551b2f462928c64be744192d06e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a735262f921121096624",
+    "url": "/static/css/main.9fe83e67.chunk.css"
+  },
+  {
+    "revision": "819bb69b36cd43b94143",
+    "url": "/static/js/2.8638967d.chunk.js"
+  },
+  {
+    "revision": "a735262f921121096624",
+    "url": "/static/js/main.2b9a0aa4.chunk.js"
+  },
+  {
+    "revision": "9eaa76e77d14d57d65a4",
+    "url": "/static/js/runtime~main.b41af966.js"
+  }
+]);
